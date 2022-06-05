039321ecdc13d80cddf229f68ce9a19eb3fc5ef3
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-06-05 20:59:35 -0300

    feat: add selected card function

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,23 +1,28 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { User as UserType } from 'types/user'
 import User from 'components/User'
+import S from './styles.module.scss'
 
 type UsersProps = {
 	users: UserType[]
 }
 
 const Users = ({ users }: UsersProps) => {
+	const [selectedUser, setSelectedUser] = useState(-1)
+
 	return (
-		<ul>
-			{users.map(({ id, name, email, birthdate, address, phone }) => (
+		<ul className={S.Wrapper}>
+			{users.map(({ id, name, email, address, phone, image }) => (
 				<User
 					key={id}
 					id={id}
 					name={name}
 					email={email}
-					birthdate={birthdate}
 					address={address}
 					phone={phone}
+					image={image}
+					selectedUser={selectedUser}
+					setSelectedUser={setSelectedUser}
 				/>
 			))}
 		</ul>
