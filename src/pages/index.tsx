b299fe6dcd9eb1acb1247345a349b608309b2e89
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-06-05 01:01:41 -0300

    feat: add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+import Users from 'components/Users'
 import type { NextPage } from 'next'
+import { FormEvent, useState } from 'react'
+import { api } from 'services/api'
+import { User } from 'types/user'
 
 const Home: NextPage = () => {
-	return <h1>ya</h1>
+	const [filter, setFilter] = useState('')
+	const [users, setUsers] = useState<User[]>([])
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault()
+		const res = await api.get(`/users?q=${filter}`)
+		const data = await res.data
+		setUsers(data)
+	}
+
+	return (
+		<header>
+			<h1>Usuários</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					value={filter}
+					onChange={(e) => setFilter(e.target.value)}
+				/>
+				&nbsp;
+				<button type='submit'>Buscar</button>
+			</form>
+
+			<Users users={users} />
+		</header>
+	)
 }
 
 export default Home
