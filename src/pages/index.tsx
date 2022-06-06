f05988f5cc3b347abab761db545997691abb1736
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-06-05 21:00:20 -0300

    fix: change home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,19 @@ import Users from 'components/Users'
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import { User } from 'types/user'
+import S from 'styles/Home.module.scss'
 
 const Home: NextPage = () => {
 	const [users, setUsers] = useState<User[]>([])
 
 	return (
-		<div>
+		<>
 			<Header />
-			<Filter setUsers={setUsers} />
-			<Users users={users} />
-		</div>
+			<main className={S.main}>
+				<Filter setUsers={setUsers} />
+				<Users users={users} />
+			</main>
+		</>
 	)
 }
 
