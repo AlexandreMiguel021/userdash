811767000931658a2be062f1fe2a532ff68f22ff
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-06-12 10:22:54 -0300

    feat: add api response message in header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
-import { memo } from 'react'
+import { UserContext } from 'contexts/UserContext'
+import { memo, useContext } from 'react'
 import S from './styles.module.scss'
 
 const Header = () => {
+	const { statusMessage } = useContext(UserContext)
 	return (
 		<header className={S.header}>
 			<h1>UserDash</h1>
+			{statusMessage && (
+				<div>
+					<span>{statusMessage}</span>
+				</div>
+			)}
 		</header>
 	)
 }
