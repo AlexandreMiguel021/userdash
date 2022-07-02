ca8679844f1db11def18c802cf09b51e501d039a
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-07-02 15:53:25 -0300

    config: add requests functions

---
+++ src/services/api/requests.ts
@@ -0,0 +1,16 @@
+import { UserFormData } from 'types/user'
+import { instance } from './config'
+
+export const getUsersRequest = (filter: string) =>
+	instance.get(`/users?q=${filter}`)
+
+export const createUserRequest = (user: UserFormData) =>
+	instance.post('/users', user)
+
+export const deleteUserByIdRequest = (selectedUser: string) =>
+	instance.delete(`/users/${selectedUser}`)
+
+export const updateUserByIdRequest = (
+	selectedUser: string,
+	user: UserFormData
+) => instance.put(`/users/${selectedUser}`, user)
