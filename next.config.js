30a0f8edeab9301bca691a61f83a175da816db34
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-07-02 15:49:39 -0300

    config: add libs and fix in settings files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
 	images: {
-		domains: ['avatars.githubusercontent.com']
+		domains: ['avatars.githubusercontent.com', 'i.pinimg.com']
+	},
+	compiler: {
+		styledComponents: true
 	}
 }
 
