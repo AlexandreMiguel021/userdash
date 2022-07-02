faa0e9a5f4a8f049c7afe9159716dad49bd2abd2
Alexandre <alexandrefreitasmiguel021@gmail.com>
2022-07-02 15:56:51 -0300

    feat: add toastfy and theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { UserContextProvider } from 'contexts/UserContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+import Header from 'components/Layout/Header'
+import { ThemeProvider } from 'styled-components'
+import theme from 'theme/theme'
+import GlobalStyles from 'theme/globals'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<UserContextProvider>
+			<ThemeProvider theme={theme}>
+				<Header />
+				<Component {...pageProps} />
+				<ToastContainer style={{ fontSize: '1.8rem' }} />
+				<GlobalStyles />
+			</ThemeProvider>
+		</UserContextProvider>
+	)
 }
 
 export default MyApp
